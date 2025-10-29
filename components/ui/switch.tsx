2afe58338545b91a21b8a77ba4ef5dd0dbaff9c3
igor <igor.srs08@gmail.com>
2025-10-29 04:55:36 -0300

    toggle again

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -15,10 +15,12 @@ const Switch = React.forwardRef<
       "data-[state=checked]:bg-gradient-to-r data-[state=checked]:from-blue-500 data-[state=checked]:to-blue-600 data-[state=checked]:border-transparent",
       "dark:data-[state=checked]:from-blue-500 dark:data-[state=checked]:to-blue-600",
       "data-[state=unchecked]:bg-slate-200 data-[state=unchecked]:border-slate-300",
-      "dark:data-[state=unchecked]:bg-slate-600 dark:data-[state=unchecked]:border-slate-500",
+      // Dark mode OFF state: maximum contrast track
+      "dark:data-[state=unchecked]:bg-slate-800 dark:data-[state=unchecked]:border-slate-700",
       "hover:data-[state=checked]:from-blue-600 hover:data-[state=checked]:to-blue-700",
       "hover:data-[state=unchecked]:bg-slate-300 hover:data-[state=unchecked]:border-slate-400",
-      "dark:hover:data-[state=unchecked]:bg-slate-500 dark:hover:data-[state=unchecked]:border-slate-400",
+      // Dark mode OFF hover: slightly lighter to indicate hover without losing contrast
+      "dark:hover:data-[state=unchecked]:bg-slate-700 dark:hover:data-[state=unchecked]:border-slate-600",
       className
     )}
     {...props}
@@ -30,7 +32,8 @@ const Switch = React.forwardRef<
         "data-[state=checked]:translate-x-[26px] data-[state=unchecked]:translate-x-[2px]",
         "bg-white shadow-[0_1px_3px_rgba(0,0,0,0.2)]",
         "data-[state=checked]:shadow-[0_2px_8px_rgba(59,130,246,0.5)]",
-        "dark:data-[state=unchecked]:bg-slate-900 dark:data-[state=unchecked]:shadow-[0_2px_4px_rgba(0,0,0,0.6)]",
+        // Dark mode OFF state: white thumb for maximum contrast
+        "dark:data-[state=unchecked]:bg-white dark:data-[state=unchecked]:shadow-[0_2px_6px_rgba(0,0,0,0.5)]",
         "dark:data-[state=checked]:bg-white dark:data-[state=checked]:shadow-[0_2px_8px_rgba(59,130,246,0.5)]"
       )}
     />
